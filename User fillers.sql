05f2ca7d91c866f43bdbe00aa256a0e1cdf1aed2
dun-stu <82878602+dun-stu@users.noreply.github.com>
2022-10-19 17:07:31 +0100

    final

---
+++ User fillers.sql
@@ -0,0 +1,10 @@
+insert into Users (Username, Password)
+Values ('Amal','Password'),
+	   ('Jamie','Password'),
+	   ('Andy','Password'),	   
+	   ('Tobi','Password'),   	   
+       ('Jenath','Password'),  
+	   ('Rohan','Password'),
+	   ('Matthew','Password'),		   
+	   ('Maryam','Password')
+
--- bookish.js
+++ bookish.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import sql from 'mssql';
+import {table} from './tablelate.js'
+import jwt from 'jsonwebtoken'
 
 const app = express()
 const port = 3000
@@ -9,6 +11,16 @@ const host = 'localhost'
 app.listen(port, host, () => {
   console.log(`Example app listening on port ${port}, ${host}`)
 })
+
+class BookClass{
+
+  constructor(BookObject){
+    this.BookID = BookObject.BookID
+    this.Title = BookObject.Title
+    this.Author = BookObject.Author
+    this.ISBN = BookObject.ISBN
+  }
+}
 
 const sqlConfig = {
   database: 'Libary management API',
@@ -25,20 +37,29 @@ const sqlConfig = {
     trustServerCertificate: true // change to true for local dev / self-signed certs
   }
 }
-async function b(){
+async function b(Query){
  try {
   // make sure that any items are correctly URL encoded in the connection string
   await sql.connect(sqlConfig)
-  
-  const result = await sql.query(`select * from Book`)
-  console.log('fog')
-  console.dir(result)
-  app.get('/', (req, res) => {
-  res.send(result.recordsets[0])
-})
+  app.get('/Books', async(req, res) => {
+    let result = await sql.query(Query)
+    const Books = result.recordsets[0].map(BookObject=>new BookClass(BookObject))
+    res.send(Books)
+  })
 
- } catch(err){console.log(err.message)}
+  app.get('/login', async(req, res) => {
+    let givenUsername = req.query.username
+    let givenPassword = req.query.password
+    let result = await sql.query(`select Password from Users where (Username = '${givenUsername}') and (Password = '${givenPassword}')`)
+    var token = jwt.sign(); //
+    console.log(token)
+    const UserRecords = result.recordsets[0]
+    res.send(UserRecords)
+  })
+
+ } catch(err){res.send(UserRecords)}
 }
 
-b()
+let Query = `select * from Book`
+b(Query)
 
